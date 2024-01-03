c36ff35cb75758f2c49301ce2417f57691c2dde5
= <alex@coolcatsnft.com>
2024-01-03 12:19:52 +0000

    feat: update pkg to just ui components

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 export * from './constants';
-export * from './canvasUtils';
-export * from './Avatar/rules';
-export * from './Avatar/utils';
 export * from './partials';
 export * from './functions';
 export * from './GlobalStyle';
@@ -10,9 +7,5 @@ export * from './Button';
 export * from './Icon';
 export * from './Dots';
 export * from './Label';
-export * from './LayeredCanvas';
-export * from './AvatarCanvas';
 export * from './Header';
 export * from './IconButton';
-
-export * as AvatarTypes from './Avatar/types';
